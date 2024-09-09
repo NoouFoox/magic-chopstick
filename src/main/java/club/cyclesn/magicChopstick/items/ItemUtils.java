fc6a81f6939a6a8b7a21cf58a9f354c391f1258e
贺俊浩 <nougat_hx@qq.com>
2024-09-09 13:32:26 +0800

    feat:runSkill

--- src/main/java/club/cyclesn/magicChopstick/items/ItemUtils.java
+++ src/main/java/club/cyclesn/magicChopstick/items/ItemUtils.java
@@ -5,19 +5,21 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
+import org.jetbrains.annotations.NotNull;
 
 public class ItemUtils {
     static public boolean isCustomItem(ItemStack item, String type) {
-        if (item == null || !item.hasItemMeta()) {
-            return false;
-        }
         ItemMeta meta = item.getItemMeta();
+        PersistentDataContainer data = null;
         if (meta != null) {
-        PersistentDataContainer data = meta.getPersistentDataContainer();
+            data = meta.getPersistentDataContainer();
+        }
+        if (data != null) {
             return type.equals(data.get(MagicChopstick.MagicChopstickKey, PersistentDataType.STRING));
         }
         return false;
     }
+
     static public boolean isMagicChopstick(ItemStack item) {
         if (item == null || !item.hasItemMeta()) {
             return false;
